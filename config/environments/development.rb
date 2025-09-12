89cc93eb8fa1928196390d2c46767269f8c04c37
Karen Barbé <info@karenbarbe.com>
2025-09-12 17:14:07 -0500

    add User model with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Allow POST authenticity on Codespaces in dev
   config.action_controller.forgery_protection_origin_check = false
+
+  # Define default url option for dev environment
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
