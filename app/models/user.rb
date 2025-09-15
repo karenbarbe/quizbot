a366e20ed977dd710cc4d30481e813feb8a8fc48
Karen Barbé <info@karenbarbe.com>
2025-09-15 16:12:55 -0500

    validate username

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :quizzes
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
